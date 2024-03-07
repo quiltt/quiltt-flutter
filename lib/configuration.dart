e11a26ec82090b09065d27f2d8b4a15547a106d5
Tom Lee <tom@quiltt.io>
2024-03-07 14:52:19 -0800

    Expose institution in Flutter SDK

--- lib/configuration.dart
+++ lib/configuration.dart
@@ -2,10 +2,12 @@ class QuilttConnectorConfiguration {
   String connectorId;
   String oauthRedirectUrl;
   String? connectionId;
+  String? institution;
 
   QuilttConnectorConfiguration({
     required this.connectorId,
     required this.oauthRedirectUrl,
     this.connectionId,
+    this.institution,
   });
 }
